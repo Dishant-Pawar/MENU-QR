f16339cf31fa3d4fe657da47a3592fa291431613
Dishant Pawar <rangerpawar6666@gmail.com>
2025-05-18 23:16:29 +0530

    er5

--- src/pageComponents/MenuPrintCreator/MenuPrintCreator.page.tsx
+++ src/pageComponents/MenuPrintCreator/MenuPrintCreator.page.tsx
@@ -195,7 +195,7 @@ export const MenuPrintCreatorPage = ({
       </div>
       <MenuPdfGenerator
         {...debouncedValues}
-        qrCodeUrl={`https://your-actual-vercel-domain.vercel.app/menu/-spice-route-bistro-pune-943`}
+        qrCodeUrl={`https://testing-code-puce.vercel.app/menu/-spice-route-bistro-pune-943/preview`}
         menuLogoImageUrl={data?.logoImageUrl ?? null}
       />
     </div>
